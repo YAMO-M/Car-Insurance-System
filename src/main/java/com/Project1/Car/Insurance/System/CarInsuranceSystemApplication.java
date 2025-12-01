1fcbdef560e5433c9a1aa9f055f8fbf4970611b2
YAMO-M <bayandamtshali2006@gmail.com>
2025-12-01 13:45:08 +0200

    Initial commit

---
+++ src/main/java/com/Project1/Car/Insurance/System/CarInsuranceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Project1.Car.Insurance.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInsuranceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInsuranceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Project1/Car/Insurance/System/CarInsuranceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Project1.Car.Insurance.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInsuranceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
